9bbd87355a039ec862db0f48816774971b74f472
Rescarra <ryn.b.escarra@gmail.com>
2017-12-17 10:21:13 -0600

    securing stylesheets

--- test/test.js
+++ test/test.js
@@ -1,52 +1,34 @@
-// USE NIGHTMARE TO ENSURE OUR LOGIN AND MEMBER PAGES ARE WORKING :)
-// ================================================================
-
+// // USE NIGHTMARE TO ENSURE OUR LOGIN AND MEMBER PAGES ARE WORKING :)
+// // ================================================================
 var Nightmare = require("nightmare");
-var expect = require("chai").expect;
 
-// describe("Codecademy", function() {
-//   // The default tests in mocha is 2 seconds.
-//   // Extending it to 30 seconds to have time to load the pages
+// STORY: As a developer nerd, I want to be able to take courses on web tech.
+new Nightmare({ show: true })
+  // Visit login page
+  .goto("http://mood-in-pixels-2180m.herokuapp.com/")
 
-//   this.timeout(30000);
-//   it("should send user to the catalog page", function(done) {
-//     // ID for the login button.
-//     Nightmare({ show: true })
-//       .goto("https://codecademy.com")
-//       // Click the catalog link
-//       .click("a[href='/learn/all']")
-//       // Evaluate the title
-//       .evaluate(function() {
-//         return document.title;
-//       })
-//       // Asset the title is as expected
-//       .then(function(title) {
-//         expect(title).to.equal("Catalog | Codecademy");
-//         done();
-//       });
-//   });
-
-//   it("should present a link to course catalog after login", function(done) {
-//     new Nightmare({ show: true })
-//       .goto("https://www.codecademy.com/login")
-//       // Enter username.
-//       .type("#user_login", "ResilD")
-//       // Enter password.
-//       .type("#login__user_password", "dummy*password")
-//       // Click the login button
-//       .click("#user_submit")
-//       // Evaluate the following selector
-//       .evaluate(function() {
-//         // Assert the "learn" link can be found
-//         return document.querySelector("a[href='/learn']");
-//       })
-//       .then(function(link) {
-//         expect(link).to.not.equal(undefined);
-//         done();
-//       });
-//   });
-
-//   it("should throw an error for fun", function() {
-//     throw new Error("Failed on purpose, just to make the Mocha output more interesting.");
-//   });
-// });
+  .screenshot("main.png")
+  .click("a[href='/login']")
+  // Enter username.
+  .type("#username-input", "username")
+  // Enter password.
+  .type("#username-password", "password")
+  // Take a screenshot of the login form.
+  .screenshot("login.png")
+  // Click login button. Always check if you've done this where necessary!
+  // It's easy to forget.
+  .click("#login")
+  // Click course catalog link.
+  .click("a[href='/members']")
+  // Take a screenshot and save it to the current directory.
+  .screenshot("daily.png")
+  // End test
+  .end()
+  // Execute commands
+  .then(function() {
+    console.log("Done!");
+  })
+  // Catch errors
+  .catch(function(err) {
+    console.log(err);
+  });
